fed28906ecb1900153174d8b76db319868903100
Arkar Min Tun <arkarmintun1@gmail.com>
2023-08-14 23:12:54 +0800

    Initial Commit

---
+++ Unit Converter/Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConverterApp.swift
+//  Unit Converter
+//
+//  Created by Arkar Min Tun on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
